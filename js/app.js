a4d18f820fffe8a163d96a7663a000ee58fc304d
ZokoApp <softwareandapp.arg@gmail.com>
2026-05-29 14:47:49 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -88,3 +88,144 @@ function populateDrivers(){
     });
 
 }
+
+const saveSunday =
+document.getElementById("saveSunday");
+
+const sundayDate =
+document.getElementById("sundayDate");
+
+btnNewSunday.addEventListener(
+"click",
+() => {
+
+    modal.classList.remove(
+    "hidden"
+    );
+
+    sundayDate.value =
+    getNextSunday();
+
+}
+);
+
+saveSunday.addEventListener(
+"click",
+saveNewSunday
+);
+
+function getNextSunday(){
+
+    const today = new Date();
+
+    const next =
+    new Date(today);
+
+    const daysUntilSunday =
+    (7 - today.getDay()) % 7;
+
+    next.setDate(
+        today.getDate() +
+        (daysUntilSunday || 7)
+    );
+
+    return next
+    .toISOString()
+    .split("T")[0];
+
+}
+
+function saveNewSunday(){
+
+    const date =
+    sundayDate.value;
+
+    const chofer1 =
+    driver1.value;
+
+    const chofer2 =
+    driver2.value;
+
+    if(
+        !date ||
+        !chofer1 ||
+        !chofer2
+    ){
+        alert(
+        "Completá todos los campos"
+        );
+        return;
+    }
+
+    if(
+        chofer1 === chofer2
+    ){
+        alert(
+        "No podés repetir chofer"
+        );
+        return;
+    }
+
+    const selected =
+    new Date(
+      date + "T12:00:00"
+    );
+
+    if(
+      selected.getDay() !== 0
+    ){
+      alert(
+      "Solo se pueden registrar domingos"
+      );
+      return;
+    }
+
+    const exists =
+    sundays.find(
+      s => s.date === date
+    );
+
+    if(exists){
+
+      alert(
+      "Ese domingo ya existe"
+      );
+
+      return;
+    }
+
+    sundays.push({
+
+      date,
+
+      drivers:[
+        Number(chofer1),
+        Number(chofer2)
+      ]
+
+    });
+
+    localStorage.setItem(
+      "sundays",
+      JSON.stringify(sundays)
+    );
+
+    document.getElementById(
+      "totalSundays"
+    ).textContent =
+    sundays.length;
+
+    document.getElementById(
+      "totalHours"
+    ).textContent =
+    sundays.length * 16;
+
+    modal.classList.add(
+      "hidden"
+    );
+
+    alert(
+      "Domingo guardado correctamente"
+    );
+
+}
